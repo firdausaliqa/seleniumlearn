7f22562dd7c3c7faf3218d46c244fb2e7a3cf3f1
M Firdaus Ali <151807788+firdausaliqa@users.noreply.github.com>
2024-05-21 14:12:42 +0700

    acd

--- sauceauto.py
+++ sauceauto.py
@@ -3,7 +3,11 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By #library untuk cari element
 from selenium.webdriver.common.keys import Keys #library untuk input key
 
+<<<<<<< HEAD
+class SaucedemoTest(unittest.Testcase):
+=======
 class SaucedemoTest(unittest.TestCase):
+>>>>>>> 5c05b4bbf98257b5bd59f7ac8ab0c73da71fd0b4
     
     def a_setUp(self):
         self.browser = webdriver.Firefox() #untuk define browser apa yg digunakan
