2cbb2e5bb416bf9df2b641452ce0baff75334219
M Firdaus Ali <151807788+firdausaliqa@users.noreply.github.com>
2024-05-21 13:25:56 +0700

    update

--- sauceauto.py
+++ sauceauto.py
@@ -3,13 +3,14 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 browser = webdriver.Firefox()
-browser.get('https://www.saucedemo.com/v1/')
+browser.get('https://demowebshop.tricentis.com/')
 
 class SaucedemoTest(unittest.Testcase):
     
     def setUp(self):
         self.browser = webdriver.Firefox
-        browser.get('https://www.saucedemo.com/')
+        browser.get('https://demowebshop.tricentis.com/')
+        elem = browser.find_element(By.ID, '')
 
 
 if __name__ == "__main__":
