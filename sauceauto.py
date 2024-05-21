5c05b4bbf98257b5bd59f7ac8ab0c73da71fd0b4
M Firdaus Ali <firdausaliwork@gmail.com>
2024-05-21 14:07:55 +0700

    aa

--- sauceauto.py
+++ sauceauto.py
@@ -1,17 +1,17 @@
-import unittest
+import unittest #adding framework jadi output lebih jelas
 from selenium import webdriver
-from selenium.webdriver.common.by import By
+from selenium.webdriver.common.by import By #library untuk cari element
+from selenium.webdriver.common.keys import Keys #library untuk input key
 
-browser = webdriver.Firefox()
-browser.get('https://demowebshop.tricentis.com/')
-
-class SaucedemoTest(unittest.Testcase):
+class SaucedemoTest(unittest.TestCase):
     
-    def setUp(self):
-        self.browser = webdriver.Firefox
-        browser.get('https://demowebshop.tricentis.com/')
-        elem = browser.find_element(By.ID, '')
+    def a_setUp(self):
+        self.browser = webdriver.Firefox() #untuk define browser apa yg digunakan
 
+    def b_register(self):
+        browser = self.browser
+        browser.get('https://demowebshop.tricentis.com/')
+        self.assertIn('Demo Web Shop', self.browser.title)
 
 if __name__ == "__main__":
     unittest.main()
